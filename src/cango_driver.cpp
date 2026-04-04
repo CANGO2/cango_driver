fe0f6ee81e6c4c1ed25c2512e2d958ea42f6f166
lee sun-kyoung <ardcxvsk@gmail.com>
2026-04-04 16:56:28 +0900

    stand

--- src/cango_driver.cpp
+++ src/cango_driver.cpp
@@ -17,81 +17,86 @@ void CangoDriver::setup() {
 
   sport_req = std::make_unique<SportClient>();
 
-/////////////////Read state from go2////////////////////
+  /////////////////Read state from go2////////////////////
   auto lowstate_cb = "lf/lowstate";
   if (HIGH_FREQ) {
     lowstate_cb = "lowstate";
   }
   state_suber = this->create_subscription<unitree_go::msg::LowState>(
-      lowstate_cb, 10, std::bind(&CangoDriver::state_cb, this, std::placeholders::_1));
-//////// ///Timer for sending request to go2//////////////////////
+      lowstate_cb, 10,
+      std::bind(&CangoDriver::state_cb, this, std::placeholders::_1));
+  //////// ///Timer for sending request to go2//////////////////////
   timer_ =
       this->create_wall_timer(std::chrono::milliseconds(20),  // 50Hz
                               std::bind(&CangoDriver::timerCallback, this));
 }
 
-void CangoDriver::state_cb(unitree_go::msg::LowState::SharedPtr data)
-  {
+void CangoDriver::state_cb(unitree_go::msg::LowState::SharedPtr data) {
+  if (INFO_IMU) {
+    // Info IMU states
+    // RPY euler angle(ZYX order respected to body frame)
+    // Quaternion
+    // Gyroscope (raw data)
+    // Accelerometer (raw data)
+    imu = data->imu_state;
 
-    if (INFO_IMU)
-    {
-      // Info IMU states
-      // RPY euler angle(ZYX order respected to body frame)
-      // Quaternion
-      // Gyroscope (raw data)
-      // Accelerometer (raw data)
-      imu = data->imu_state;
+    RCLCPP_INFO(this->get_logger(),
+                "Euler angle -- roll: %f; pitch: %f; yaw: %f", imu.rpy[0],
+                imu.rpy[1], imu.rpy[2]);
+    RCLCPP_INFO(this->get_logger(),
+                "Quaternion -- qw: %f; qx: %f; qy: %f; qz: %f",
+                imu.quaternion[0], imu.quaternion[1], imu.quaternion[2],
+                imu.quaternion[3]);
+    RCLCPP_INFO(this->get_logger(), "Gyroscope -- wx: %f; wy: %f; wz: %f",
+                imu.gyroscope[0], imu.gyroscope[1], imu.gyroscope[2]);
+    RCLCPP_INFO(this->get_logger(), "Accelerometer -- ax: %f; ay: %f; az: %f",
+                imu.accelerometer[0], imu.accelerometer[1],
+                imu.accelerometer[2]);
+  }
 
-      RCLCPP_INFO(this->get_logger(), "Euler angle -- roll: %f; pitch: %f; yaw: %f", imu.rpy[0], imu.rpy[1], imu.rpy[2]);
-      RCLCPP_INFO(this->get_logger(), "Quaternion -- qw: %f; qx: %f; qy: %f; qz: %f",
-                  imu.quaternion[0], imu.quaternion[1], imu.quaternion[2], imu.quaternion[3]);
-      RCLCPP_INFO(this->get_logger(), "Gyroscope -- wx: %f; wy: %f; wz: %f", imu.gyroscope[0], imu.gyroscope[1], imu.gyroscope[2]);
-      RCLCPP_INFO(this->get_logger(), "Accelerometer -- ax: %f; ay: %f; az: %f",
-                  imu.accelerometer[0], imu.accelerometer[1], imu.accelerometer[2]);
-    }
+  if (INFO_MOTOR) {
+    // Info motor states
+    // q: angluar (rad)
+    // dq: angluar velocity (rad/s)
+    // ddq: angluar acceleration (rad/(s^2))
+    // tau_est: Estimated external torque
 
-    if (INFO_MOTOR)
-    {
-      // Info motor states
-      // q: angluar (rad)
-      // dq: angluar velocity (rad/s)
-      // ddq: angluar acceleration (rad/(s^2))
-      // tau_est: Estimated external torque
-
-      for (int i = 0; i < 12; i++)
-      {
-        motor[i] = data->motor_state[i];
-        RCLCPP_INFO(this->get_logger(), "Motor state -- num: %d; q: %f; dq: %f; ddq: %f; tau: %f",
-                    i, motor[i].q, motor[i].dq, motor[i].ddq, motor[i].tau_est);
-      }
-    }
-
-    if (INFO_FOOT_FORCE)
-    {
-      // Info foot force value (int not true value)
-      for (int i = 0; i < 4; i++)
-      {
-        foot_force[i] = data->foot_force[i];
-        foot_force_est[i] = data->foot_force_est[i];
-      }
-
-      RCLCPP_INFO(this->get_logger(), "Foot force -- foot0: %d; foot1: %d; foot2: %d; foot3: %d",
-                  foot_force[0], foot_force[1], foot_force[2], foot_force[3]);
-      RCLCPP_INFO(this->get_logger(), "Estimated foot force -- foot0: %d; foot1: %d; foot2: %d; foot3: %d",
-                  foot_force_est[0], foot_force_est[1], foot_force_est[2], foot_force_est[3]);
-    }
-
-    if (INFO_BATTERY)
-    {
-      // Info battery states
-      // battery current
-      // battery voltage
-      battery_current = data->power_a;
-      battery_voltage = data->power_v;
-
-      RCLCPP_INFO(this->get_logger(), "Battery state -- current: %f; voltage: %f", battery_current, battery_voltage);
+    for (int i = 0; i < 12; i++) {
+      motor[i] = data->motor_state[i];
+      RCLCPP_INFO(this->get_logger(),
+                  "Motor state -- num: %d; q: %f; dq: %f; ddq: %f; tau: %f", i,
+                  motor[i].q, motor[i].dq, motor[i].ddq, motor[i].tau_est);
     }
   }
+
+  if (INFO_FOOT_FORCE) {
+    // Info foot force value (int not true value)
+    for (int i = 0; i < 4; i++) {
+      foot_force[i] = data->foot_force[i];
+      foot_force_est[i] = data->foot_force_est[i];
+    }
+
+    RCLCPP_INFO(this->get_logger(),
+                "Foot force -- foot0: %d; foot1: %d; foot2: %d; foot3: %d",
+                foot_force[0], foot_force[1], foot_force[2], foot_force[3]);
+    RCLCPP_INFO(
+        this->get_logger(),
+        "Estimated foot force -- foot0: %d; foot1: %d; foot2: %d; foot3: %d",
+        foot_force_est[0], foot_force_est[1], foot_force_est[2],
+        foot_force_est[3]);
+  }
+
+  if (INFO_BATTERY) {
+    // Info battery states
+    // battery current
+    // battery voltage
+    battery_current = data->power_a;
+    battery_voltage = data->power_v;
+
+    RCLCPP_INFO(this->get_logger(), "Battery state -- current: %f; voltage: %f",
+                battery_current, battery_voltage);
+  }
+}
 
 void CangoDriver::controlCallback(
     const cango_msgs::msg::RobotControl::ConstSharedPtr &msg) {
@@ -108,7 +113,8 @@ void CangoDriver::timerCallback() {
       unitree_api::msg::Request gait_req;
       unitree_api::msg::Request gait_req1;
       unitree_api::msg::Request gait_req2;
-      
+      unitree_api::msg::Request gait_req3;
+
       sport_req->StandUp(gait_req);
       req_puber->publish(gait_req);
 
@@ -116,8 +122,10 @@ void CangoDriver::timerCallback() {
       req_puber->publish(gait_req1);
 
       sport_req->SwitchGait(gait_req2, 3);
-      sport_req->ContinuousGait(gait_req2, true);
       req_puber->publish(gait_req2);
+
+      sport_req->ContinuousGait(gait_req3, true);
+      req_puber->publish(gait_req3);
       gait_set = true;
     }
     unitree_api::msg::Request fresh_req;
